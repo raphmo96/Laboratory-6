6ae0cc2b361cb0cda3c147e17880b909eef13404
Raphael Morin <1810028@bart.ca>
2018-05-09 12:42:25 -0400

    AI bug fixing makin it work

--- Mastermind/GameMaster.h
+++ Mastermind/GameMaster.h
@@ -6,15 +6,20 @@ class GameMaster
 	char maxColors[8]{ 'Y','B','P','O','G','R','C','T' };
 	char* colors;
 	char* masterKey;
+	char* userKey;
 	int rounds;
 
 	void Instructions();
 	void CreateGrid(const int&, const int&);
 	void DrawGrid();
-	void UpdateGrid(char*);
+	void UpdateGrid();
 	char* AskUserInput();
-	std::string PrintColor(char*);
-
+	std::string PrintColor(char*&);
+	bool CheckWin();
+	void CreateKey();
+	void GameState(bool&);
+	char* RemoveChar(char*&, int&, int&);
+	std::uniform_int_distribution<> *dice;
 	std::map<char, std::string> dictionnary = { {'Y',"Yellow"},{ 'B',"Blue" },{ 'P',"Purple" },{ 'O',"Orange" },{ 'G',"Green" },{ 'R',"Red" },{ 'C',"Cyan" },{ 'T',"Turquoise" } };
 public:
 	GameMaster(int, int);
